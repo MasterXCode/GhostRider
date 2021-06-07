94937849986f6923a1725ad31c33ec66f4ff7387
MASTERX <seifsherifssv@gmail.com>
2021-06-07 14:23:11 +0000

    Add files via upload

---
+++ setup-linux.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3
+sudo python3 -m pip install --upgrade pip
+sudo pip3 install Flask==1.0.2 python-nmap==0.6.4 PySocks==1.7.1 scapy==2.4.4 bleach==3.2.1 colorama==0.4.4 requests==2.25.1 thread6:0.2.0 terminable_thread:0.7.1 thread-queue:0.2.3 hread_pool:0.9 thread-task:0.9.7
